0d9f631558181bb4aa56c9155ed7e07615d14ad6
Balmaaa <gsbalmaceda@up.edu.ph>
2023-12-09 01:38:06 +0800

    Added New Functions
    Code now includes functions such as converting all user inputs into 1 string output.
    
    Added Items to system now generates its own SKU Identifier. Although the Category is still needed to be fixed.

--- Application/src/main/java/upm/cs23/grp1/application/Controller_1.java
+++ Application/src/main/java/upm/cs23/grp1/application/Controller_1.java
@@ -1,14 +1,20 @@
 package upm.cs23.grp1.application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Random;
 
 
 public class Controller_1
@@ -41,6 +47,46 @@ public class Controller_1
         Scene = new Scene(Root);
         Stage.setScene(Scene);
         Stage.show();
+    }
+
+    @FXML
+    private Text AddedItemOutput;
+
+    @FXML
+    private Text GeneratedSKU;
+
+    @FXML
+    private TextField ItemName;
+
+    @FXML
+    private TextField Category;
+
+    @FXML
+    private TextField Brand;
+
+    @FXML
+    private TextField WeightVolume;
+
+    @FXML
+    private TextField Quantity;
+
+    @FXML
+    private TextArea AddDescription;
+
+
+    @FXML
+    private void AddItemToDisplay()
+    {
+        String ItemNameText = ItemName.getText();
+        String CategoryText = Category.getText();
+        String BrandText = Brand.getText();
+        String WeightVolumeText = WeightVolume.getText();
+        int QuantityNumber = Integer.parseInt(Quantity.getText());
+        int SKUItemVal = (int) (Math.random() * (9999));
+        String SKUItem = ItemNameText.substring(0, Math.min(ItemNameText.length(), 3)).toUpperCase();
+
+        AddedItemOutput.setText(ItemNameText + " | " + CategoryText + " | " + BrandText + " | " + WeightVolumeText + " | " + QuantityNumber);
+        GeneratedSKU.setText(CategoryText + "/" + SKUItem + "-" + SKUItemVal);
     }
 
 
