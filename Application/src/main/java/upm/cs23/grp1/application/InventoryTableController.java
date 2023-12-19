bf4e8d9c7507b710aed4170621573dbf2c80fe2d
Balmaaa <gsbalmaceda@up.edu.ph>
2023-12-20 00:33:32 +0800

    Removed Unnecessary JavaFX Imports
    Removed Unused Imports in InventoryTableController.java

--- Application/src/main/java/upm/cs23/grp1/application/InventoryTableController.java
+++ Application/src/main/java/upm/cs23/grp1/application/InventoryTableController.java
@@ -2,19 +2,11 @@ package upm.cs23.grp1.application;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class InventoryTableController
 {
